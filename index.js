b3b983a2bc5712e113e835735904a264d4c36030
laufoes <foeslaura@gmail.com>
2022-07-15 22:04:49 -0300

    Page structure for mobile finished. Content needs adjustments

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const botaoMenu = document.querySelector('.header__menu')
 const menu = document.querySelector('.menu-lateral')
 
 botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-lateral--active')
+    menu.classList.toggle('menu-lateral--ativo')
 });
